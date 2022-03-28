1453c5f51ab046981edabbd23bc7032245b43042
jennypc <36174275+jennypc@users.noreply.github.com>
2022-03-28 15:58:21 -0600

    apiFake
    Se subio el proyecto

---
+++ api-login/db.js
@@ -0,0 +1,17 @@
+{
+    "usuarios" = [
+        {
+            "id":"1",
+            "username":"Jennypc",
+            "password":"827ccb0eea8a706c4c34a16891f84e7b",
+
+        },
+
+        {
+            "id":"2",
+            "username":"Pamela",
+            "password":"827ccb0eea8a706c4c34a16891f84e7b",
+
+        }
+    ]
+}
